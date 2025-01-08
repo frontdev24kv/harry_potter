6fee9f9060f229cffeaff8853953821fc38b35ed
frontdev24kv <kv.frontdev24@gmail.com>
2025-01-08 23:20:55 +0200

    fixed card shadow effect on student page

--- scripts/students.js
+++ scripts/students.js
@@ -3,7 +3,7 @@ const hoverBtn = document.getElementById("hover-btn")
 
 const harry = document.getElementById("harry")
 const hiddenItem = document.querySelector(".hover-item")
-const cardInfo = document.getElementById("card-info")
+const cardMask = document.getElementById("card-mask")
 
 
 const allStudentsUrl = "https://hp-api.onrender.com/api/characters/students"
@@ -12,14 +12,14 @@ const allStudentsUrl = "https://hp-api.onrender.com/api/characters/students"
 cardBtn.addEventListener("click", () => {
     setTimeout(() => {
         harry.style.display = "none"
-        cardInfo.style.display = "none"
+        cardMask.style.display = "none"
         hiddenItem.style.display = "block"
     }, 600)
 })
 hoverBtn.addEventListener("click", () => {  
     setTimeout(() => {
         harry.style.display = "block"
-        cardInfo.style.display = "block"
+        cardMask.style.display = "block"
         hiddenItem.style.display = "none"
     }, 600)  
 })

df2ed549e55aa8425f01cd22dbb176466af146e9
frontdev24kv <kv.frontdev24@gmail.com>
2025-01-08 20:56:06 +0200

    add employees card with descriptions, another changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const studentsBtn = document.getElementById("students-btn");
 const employeesBtn = document.getElementById("employees-btn");
+const housesBtn = document.getElementById("houses-btn");
 
 studentsBtn.addEventListener("click", () => {
     window.location.assign("./site/students.html")
@@ -7,5 +8,9 @@ studentsBtn.addEventListener("click", () => {
 
 employeesBtn.addEventListener("click", () => {
     window.location.assign("./site/employees.html")
+})
+
+housesBtn.addEventListener("click", () => {
+    window.location.assign("./site/house.html")
 })
 
